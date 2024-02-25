44735770fae63d6c19348837a599f4f9df9b9f8a
mnsh4 <hugo.orlando2050@gmail.com>
2024-02-25 19:58:58 -0300

    :tada: add comments microservice

---
+++ comments/server.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response } from "npm:express@^4.17";
+import { randomBytes } from "https://deno.land/std@0.91.0/node/crypto.ts";
+
+const app = express();
+const PORT = 5001;
+
+app.use(express.json());
+
+const commentsByPostId = {};
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Welcome to the Comments API!");
+});
+
+app.get("/posts/:id/comments", (req: Request, res: Response) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", (req: Request, res: Response) => {
+  const commentId = randomBytes(4).toString("hex");
+
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content });
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on http://localhost:${PORT}/`);
+});
