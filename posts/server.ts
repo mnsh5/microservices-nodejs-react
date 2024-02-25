04aa28f76e2dac65876376cb582233fcf985a1d9
mnsh4 <hugo.orlando2050@gmail.com>
2024-02-25 19:22:16 -0300

    :rocket: add new features

--- posts/server.ts
+++ posts/server.ts
@@ -1,12 +1,29 @@
 import express, { Request, Response } from "npm:express@^4.17";
+import { v4 } from 'https://deno.land/std/uuid/mod.ts';
 
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 app.use(express.json());
 
+const posts = {};
+
 app.get("/", (_req: Request, res: Response) => {
   res.send("Welcome to the Post API!");
+});
+
+app.get("/posts", (_req: Request, res: Response) => {
+  res.send(posts);
+});
+
+app.post("/posts", (req: Request, res: Response) => {
+  const id = v4.generate();
+  const { title } = req.body;
+  posts[id] = {
+    id,
+    title,
+  };
+  res.status(201).send(posts[id]);
 });
 
 app.listen(PORT, () => {
