40752346a419818e1849da6c3cc578b517062219
mnsh5 <hugo.orlando2050@gmail.com>
2024-02-29 16:58:09 -0300

    :rocket: add new features

---
+++ client/src/PostCreate.tsx
@@ -0,0 +1,13 @@
+export const PostCreate = () => {
+  return (
+    <>
+      <div>
+        <form className="form-group" action="">
+          <label>Title</label>
+          <input className="form-control" type="text" />
+        </form>
+      </div>
+      <button className="btn btn-primary">Submit</button>
+    </>
+  );
+};
