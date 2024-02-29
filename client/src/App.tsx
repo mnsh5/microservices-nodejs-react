be0d25a14e4c20643309c5ee31652cfad5372dca
mnsh5 <hugo.orlando2050@gmail.com>
2024-02-29 17:24:24 -0300

    :sparkles: add new features

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,10 +6,10 @@ function App() {
 
 
   return (
-    <>
+    <div className='container mx-auto px-4 my-2.5'>
       <h1>Create Post</h1>
       <PostCreate/>
-    </>
+    </div>
   )
 }
 
--- client/src/PostCreate.tsx
+++ client/src/PostCreate.tsx
@@ -1,13 +1,35 @@
+import axios from "axios";
+import { useState } from "react";
+
 export const PostCreate = () => {
+  const [title, setTitle] = useState("");
+
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await axios.post("http://localhost:5000/posts", {
+      title,
+    });
+    setTitle("");
+  };
+
   return (
     <>
-      <div>
-        <form className="form-group" action="">
-          <label>Title</label>
-          <input className="form-control" type="text" />
+      <div className="mt-4">
+        <form className="" onSubmit={onSubmit}>
+          <label className="block">
+            <span className="text-gray-700">Title</span>
+            <input
+              className="form-input mt-1 block w-1/2 border-current outline-pink-500"
+              placeholder="Post"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </label>
         </form>
       </div>
-      <button className="btn btn-primary">Submit</button>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-2">
+        Submit
+      </button>
     </>
   );
 };
